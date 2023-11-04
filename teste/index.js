5ae1b8ee5af39bdd06e0b66651ab89fbc39af127
OutLaio <rlaio.ege@gmail.com>
2023-11-04 03:04:37 -0300

    Conexão com banco de dados

--- teste/index.js
+++ teste/index.js
@@ -4,8 +4,40 @@ const sequelize = new Sequelize('caelum', 'root', 'zepeto222', {
     dialect: 'mysql'
 });
 
-sequelize.authenticate().then(()=>{
-    console.log("Conectado com sucesso");
-}). catch((e)=>{
-    console.log("Falha ao se conectar: " + e.message);
+const Postagem = sequelize.define('postagens',{
+    titulo:{
+        type: Sequelize.STRING
+    },
+    conteudo:{
+        type: Sequelize.TEXT
+    }
 });
+
+Postagem.create({
+    titulo: "PRIMEIRO",
+    conteudo: "Inserindo dados com js"
+});
+
+const Usuario = sequelize.define('usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+Usuario.create({
+    nome: "Laio",
+    sobrenome: "Rodrigues",
+    idade: 23,
+    email: "laio@laio.com"
+});
+
+
